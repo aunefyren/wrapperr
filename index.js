e0f8a8a5b0b79b9af5d61669810c5d7f0c5a6717
aunefyren <oystein.sverre@gmail.com>
2020-12-19 17:10:59 +0100

    First version
    The first version of my script, modified to remove sensetive information.

---
+++ index.js
@@ -0,0 +1,47 @@
+function search_button(string) {
+    $('#search_get').html(string);
+}
+
+function time_days(seconds_input) {
+    var seconds = Number(seconds_input);
+    var days = seconds * 1.15741E-5;
+
+    var hours = String(days).split(".");
+    var hours_str = "0." + hours[1];
+    var hours_int = Number(hours_str) * 24.0;
+
+    var minutes = String(hours_int).split(".");
+    var minutes_str = "0." + minutes[1];
+    var minutes_int = Number(minutes_str) * 60.0;
+
+    var days_form = String(days).split(".");
+    var hours_form = String(hours_int).split(".");
+    var minutes_form = String(minutes_int).split(".");
+
+    var final = [Number(days_form[0]), Number(hours_form[0]), Number(minutes_form[0])];
+    return final;
+}
+
+function time_hours(seconds_input) {
+    var seconds = Number(seconds_input);
+    var hours_int = Number(seconds) * 0.0002777778;
+
+    var minutes = String(hours_int).split(".");
+    var minutes_str = "0." + minutes[1];
+    var minutes_int = Number(minutes_str) * 60.0;
+
+    var hours_form = String(hours_int).split(".");
+    var minutes_form = String(minutes_int).split(".");
+
+    var final = [Number(hours_form[0]), Number(minutes_form[0])];
+    return final;
+}
+
+$(document).on('submit', '#stats_form', function(){
+
+    search_button("SEARCHING...");
+
+    $.getScript("get_stats_2020.js", function(){
+    });
+
+});
