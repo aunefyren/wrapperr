fd6d857060addc7ef7d01bc711f958397eb57e40
Aune <githubcontact.handyman356@passmail.net>
2024-12-15 16:18:22 +0100

    URI encode bearer tokens

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,4 +1,5 @@
 function set_cookie(cname, cvalue, exdays) {
+    cvalue = encodeURI(cvalue)
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
